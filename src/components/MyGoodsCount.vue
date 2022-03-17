514f4b64c77e3e0ec1a9daaf7137a8b8accb1d4f
19990710simplelife <1753848616@qq.com>
2022-03-17 17:00:15 +0800

    环境准备

--- src/components/MyGoodsCount.vue
+++ src/components/MyGoodsCount.vue
@@ -1,10 +1,10 @@
 <!--
  * @Author: your name
- * @Date: 2022-03-17 16:51:17
- * @LastEditTime: 2022-03-17 16:56:11
- * @LastEditors: your name
+ * @Date: 2022-03-17 16:57:22
+ * @LastEditTime: 2022-03-17 16:57:22
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \shopcar\src\App.vue
+ * @FilePath: \shopcar\src\components\MyGoodsCount.vue
 -->
 <template>
   <div></div>
--- src/components/MyGoodsFooter.vue
+++ src/components/MyGoodsFooter.vue
@@ -1,18 +1,21 @@
 <!--
  * @Author: your name
- * @Date: 2022-03-17 16:51:17
- * @LastEditTime: 2022-03-17 16:56:11
- * @LastEditors: your name
+ * @Date: 2022-03-17 16:57:39
+ * @LastEditTime: 2022-03-17 16:57:39
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \shopcar\src\App.vue
+ * @FilePath: \shopcar\src\components\MyGoodsFooter.vue
 -->
 <template>
   <div></div>
 </template>
 
 <script>
-export default {};
+export default {
+
+}
 </script>
 
 <style>
+
 </style>
